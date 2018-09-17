fb025566e21bb364f92adeadc360fd434ffdbd03
leo <942510346@qq.com>
2018-09-17 10:29:58 +0800

    update ch6
    Signed-off-by: leo <942510346@qq.com>

---
+++ ch6/ch5.py
@@ -0,0 +1,10 @@
+
+# 每个字典都有一个name和一个occupations列表.
+
+cleese = {}
+palin = dict()
+
+cleese['Name'] = 'John cleese'
+cleese['Occupations'] = ['actor', 'comedian', 'writer', 'film producer']
+
+palin = {'Name': 'Michael palin', 'Occupations': ['actor', 'comedian', 'writer', 'film producer']}
